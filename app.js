8ee019b39ebb7aaf652ee95a92e442c9f72d9adf
Yarega <yayasoles@gmail.com>
2022-06-29 17:51:00 +0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-setInterval(()=>{
-   var date=new Date();
-
-    document.querySelector('.budget__value').innerHTML=`${date.getHours()} : ${date.getMinutes()}  :  ${date.getSeconds()}  :  ${date.getMilliseconds()}`;
-},100)
